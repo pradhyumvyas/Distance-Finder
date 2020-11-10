0b33a911c55d8d8b9529d5ac24fdc6b939120433
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-11-11 00:02:50 +0530

    Done with Map

--- measurement/utils.py
+++ measurement/utils.py
@@ -2,7 +2,14 @@ from django.contrib.gis.geoip2 import GeoIP2
 
 # Helper Functions
 
-
+def get_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    print("IP is : ", x_forwarded_for)
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 def get_geo(ip):
     g = GeoIP2()
@@ -12,3 +19,18 @@ def get_geo(ip):
     lat,lon = g.lat_lon(ip)
 
     return country,city, lat, lon
+
+def get_center_coordinates(laA,loA, laB=None, loB=None):
+    cord = (laA, loA)
+    if laB:
+        cord = [(laA+laB)/2, (loA+loB)/2]
+
+    return cord
+
+def get_zoom(distance):
+    if distance<=100:
+        return 8
+    elif distance>100 and distance<=5000:
+        return 4
+    else:
+        return 2
--- measurement/views.py
+++ measurement/views.py
@@ -3,7 +3,7 @@ from .models import Measurement
 from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-from .utils import get_geo
+from .utils import get_geo, get_center_coordinates, get_zoom, get_ip_address
 import folium
 
 # Create your views here.
@@ -11,12 +11,15 @@ import folium
 
 def calculateDistanceView(request):
     obj = get_object_or_404(Measurement, id=1)
+    distance = None
+    destination = None
     
     form = MeasurementModelForm(request.POST or None)
     geolocator = Nominatim(user_agent='measurement')
 
     # ip = '72.14.207.99' 
-    ip = '47.247.191.26'
+    ip = '2409:4043:395:5427:e06b:a9f8:6a9f:b220'
+    ip_ = get_ip_address(request)
     country,city,lat, lon = get_geo(ip)
 
 
@@ -35,7 +38,12 @@ def calculateDistanceView(request):
 
     # Initial Folium Map 
 
-    m = folium.Map(width=800, height=500, location=pointA)
+    m = folium.Map(width=900, height=630, 
+                    location=get_center_coordinates(l_latitude,l_longitude),)
+
+    # Location Marker
+    folium.Marker([l_latitude, l_longitude], tooltip="Click here for more", popup=city['city'], 
+    icon=folium.Icon(color="purple")).add_to(m)
 
 
     if form.is_valid():
@@ -44,21 +52,49 @@ def calculateDistanceView(request):
         destination = geolocator.geocode(destination_)
         # print(destination)
 
+
+
         # Destination Coordination 
+
         d_latitude = destination.latitude
         d_longitude = destination.longitude
 
         pointB = (d_latitude, d_longitude)
 
+
         # Distance Calculation 
+
         distance = round(geodesic(pointA, pointB).km, 2)
+
+        # Folium Map Modification
+        m = folium.Map(width=800, height=500, 
+                location=get_center_coordinates(l_latitude,l_longitude,d_latitude, d_longitude),
+                zoom_start=get_zoom(distance))
+
+        # Location Marker 
+        folium.Marker([l_latitude, l_longitude], tooltip="Click here for more", popup=city['city'], 
+        icon=folium.Icon(color="purple")).add_to(m)
+
+        # Destination Marker
+        folium.Marker([d_latitude, d_longitude], tooltip="Click here for more", 
+        popup=destination, icon=folium.Icon(color="green", icon='cloud')).add_to(m)
+
+
+        # Draw the Line b/w location and destination
+
+        line = folium.PolyLine(locations=[pointA, pointB], weight=5, color="blue")
+        m.add_child(line)
 
         instance.location = location
         instance.distance = distance
         instance.save()
 
+    m = m._repr_html_()
+    # distance = None
+
     context = {
-        'distance':obj,
+        'distance':distance,
+        'destination':destination,
         'form':form,
         'map':m
     }
